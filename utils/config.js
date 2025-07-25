635f05ed452014bf93f5e8082321654a7ed16d2b
Hellnwo <hellenwo12@gmail.com>
2025-07-24 23:03:34 -0400

    Test Fix

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,6 @@
-const { JWT_SECRET = "some-secret-key"} = process.env;
+const { JWT_SECRET = "some-secret-key" } = process.env;
 
 module.exports = {
-  JWT_SECRET
+  JWT_SECRET,
+  MONGODB_URI: process.env.MONGODB_URI,
 };
